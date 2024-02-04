fcba7dd0a9e61d02d148cf102c530fbdc1ac4771
barakamwakisha <barakamwakisha@gmail.com>
2024-02-04 21:32:14 +0300

    add util tests

--- src/util/phone-utils.ts
+++ src/util/phone-utils.ts
@@ -9,7 +9,7 @@ export function formatPhoneNumber(number: string) {
 }
 
 export function isSafaricomNumber(number: string): boolean {
-    const safaricomRegex = /^(?:\+254|254|0)((1|7)(?:(?:[0-9][0-9])|(?:[0-9][0-9][0-9]))[0-9]{6})$/;
+    const safaricomRegex = /^(?:\+254|254|0)?(7[0124578][0-9]|74[0-6]|75[7-9]|76[89]|790|11[0-5])\d{6}$/;
     return safaricomRegex.test(number);
 }
 
