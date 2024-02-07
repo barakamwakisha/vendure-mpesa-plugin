988034fde41b4651359f3a7650c254b23d2eab61
barakamwakisha <barakamwakisha@gmail.com>
2024-02-07 14:39:30 +0300

    add build script. update eslintignore

--- src/api/callback-webhook.controller.ts
+++ src/api/callback-webhook.controller.ts
@@ -1,33 +1,41 @@
-import { Controller, Body, Post, Get } from '@nestjs/common';
-import { STKCallbackPayload } from '../types';
-import { CALLBACK_URL_ENDPOINT } from '../constants';
-import { MpesaService } from '../service/mpesa.service';
-import { ChannelService, LanguageCode, Logger, RequestContext } from '@vendure/core';
+import { Body, Controller, Post } from "@nestjs/common"
+import {
+    ChannelService,
+    LanguageCode,
+    Logger,
+    RequestContext
+} from "@vendure/core"
+
+import { CALLBACK_URL_ENDPOINT } from "../constants"
+import { MpesaService } from "../service/mpesa.service"
+import { STKCallbackPayload } from "../types"
 
 @Controller(CALLBACK_URL_ENDPOINT)
 export class CallbackWebhookController {
     constructor(
         private channelService: ChannelService,
         private mpesaService: MpesaService
-    ) { }
+    ) {}
 
     @Post()
     async handleCallback(@Body() payload: STKCallbackPayload) {
-        const { CheckoutRequestID, ResultCode } = payload.Body.stkCallback;
-        Logger.info(`Received Mpesa callback for transaction ${CheckoutRequestID} with status ${ResultCode}`);
+        const { CheckoutRequestID, ResultCode } = payload.Body.stkCallback
+        Logger.info(
+            `Received Mpesa callback for transaction ${CheckoutRequestID} with status ${ResultCode}`
+        )
 
-        const ctx = await this.createRequestContext();
-        this.mpesaService.updateTransactionStatus(ctx, CheckoutRequestID);
+        const ctx = await this.createRequestContext()
+        this.mpesaService.updateTransactionStatus(ctx, CheckoutRequestID)
     }
 
     private async createRequestContext(): Promise<RequestContext> {
-        const channel = await this.channelService.getDefaultChannel();
+        const channel = await this.channelService.getDefaultChannel()
         return new RequestContext({
-            apiType: 'admin',
+            apiType: "admin",
             isAuthorized: true,
             authorizedAsOwnerOnly: false,
             channel,
             languageCode: LanguageCode.en
-        });
+        })
     }
 }
--- src/config/mpesa-eligibility-checker.ts
+++ src/config/mpesa-eligibility-checker.ts
@@ -1,23 +1,33 @@
-import { LanguageCode, PaymentMethodEligibilityChecker } from "@vendure/core";
-import { formatPhoneNumber, getPhoneNumberFromOrder, isSafaricomNumber } from "../util/phone-utils";
+import { LanguageCode, PaymentMethodEligibilityChecker } from "@vendure/core"
+
+import {
+    formatPhoneNumber,
+    getPhoneNumberFromOrder,
+    isSafaricomNumber
+} from "../util/phone-utils"
 
 export const mpesaEligibilityChecker = new PaymentMethodEligibilityChecker({
-    code: 'mpesa-eligibility-checker',
-    description: [{ languageCode: LanguageCode.en, value: 'Check whether Mpesa supports the payment' }],
+    code: "mpesa-eligibility-checker",
+    description: [
+        {
+            languageCode: LanguageCode.en,
+            value: "Check whether Mpesa supports the payment"
+        }
+    ],
     args: {},
-    check: async (ctx, order, args) => {
-        const totalLessThanThreshold = order.totalWithTax < 50000000;
-        if (!totalLessThanThreshold) return false;
+    check: async (_, order) => {
+        const totalLessThanThreshold = order.totalWithTax < 50000000
+        if (!totalLessThanThreshold) return false
 
-        const customerPhoneNumber = getPhoneNumberFromOrder(order);
-        if (!customerPhoneNumber) return false;
+        const customerPhoneNumber = getPhoneNumberFromOrder(order)
+        if (!customerPhoneNumber) return false
 
-        const formattedNumber = formatPhoneNumber(customerPhoneNumber);
-        if (!formattedNumber) return false;
+        const formattedNumber = formatPhoneNumber(customerPhoneNumber)
+        if (!formattedNumber) return false
 
-        const isMpesaNumber = isSafaricomNumber(formattedNumber);
-        if (!isMpesaNumber) return false;
+        const isMpesaNumber = isSafaricomNumber(formattedNumber)
+        if (!isMpesaNumber) return false
 
-        return true;
+        return true
     }
-});
+})
